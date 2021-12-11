132b350a96224a09d323bbeaefcc0637b1eb3c05
Escudero Martial <martial.escudero@gmail.com>
2021-12-12 00:23:01 +0100

    fix responsive font link

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,6 @@ export default {
     meta: [
       { charset: 'utf-8' },
 
-
       { name: 'viewport',     content: 'width=device-width, initial-scale=1' },
       { name: 'author',       content: 'Martial Escudero' },
       { name: 'copyright',    content: 'Martial Escudero' },
@@ -27,6 +26,9 @@ export default {
     link: [
       { rel: 'icon',                          type: 'image/x-icon',   href: '/favicon.ico' },
       { rel: 'apple-touch-icon-precomposed',  type: 'image/x-icon',   href: 'https://res.cloudinary.com/do5ghqhjj/image/upload/v1638793635/apple_icon_6523101884.png' },
+      { rel: 'preconnect',                    href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect',                    href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet',                    href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,700;0,900;1,300;1,400;1,600;1,700;1,800&display=swap' },
     ],
   },
 
