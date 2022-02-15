a5449a99a3423f593a295d632fb104783c2fcf49
Escudero Martial <martial.escudero@gmail.com>
2022-02-15 11:21:03 +0100

    +1 labo

--- pages/labo.vue
+++ pages/labo.vue
@@ -3,32 +3,37 @@
     <div class="m-auto text-md xl:mx-auto">
       <div id="labo">
         <p class="text-center elementlabo" data-aos="fade-up">
-          <a rel="noopener" target="_blank" href="https://link.martialescudero.com" class="effect labotext6 xl:text-6xl sm:text-4xl">
+          <a rel="noopener" target="_blank" href="https://music.martialescudero.com/" class="effect labotext7 xl:text-5xl sm:text-4xl">
+            Album and vinyl library
+          </a>
+        </p> 
+        <p class="text-center elementlabo" data-aos="fade-up">
+          <a rel="noopener" target="_blank" href="https://link.martialescudero.com" class="effect labotext6 xl:text-5xl sm:text-4xl">
             Social Media Sharing Link
           </a>
         </p> 
         <p class="text-center elementlabo" data-aos="fade-up">
-          <NuxtLink rel="noopener" target="_blank" to="/contact" class="effect labotext5 xl:text-6xl sm:text-4xl">
+          <NuxtLink rel="noopener" target="_blank" to="/contact" class="effect labotext5 xl:text-5xl sm:text-4xl">
             Contact Form Single
           </NuxtLink>
         </p>       
         <p class="text-center elementlabo" data-aos="fade-up">
-          <NuxtLink rel="noopener" target="_blank" to="labo/meteo.html" class="effect labotext4 xl:text-6xl sm:text-4xl">
+          <NuxtLink rel="noopener" target="_blank" to="labo/meteo.html" class="effect labotext4 xl:text-5xl sm:text-4xl">
             API Météo
           </NuxtLink>
         </p>
         <p class="text-center elementlabo" data-aos="fade-up">
-          <NuxtLink rel="noopener" target="_blank" to="labo/hexaclock.html" class="effect labotext3 xl:text-6xl sm:text-4xl">
+          <NuxtLink rel="noopener" target="_blank" to="labo/hexaclock.html" class="effect labotext3 xl:text-5xl sm:text-4xl">
             HexaClock
           </NuxtLink>
         </p>
         <p class="text-center elementlabo" data-aos="fade-up">
-          <NuxtLink rel="noopener" target="_blank" to="labo/color-palette-selector.html" class="effect labotext2 xl:text-6xl sm:text-4xl">
+          <NuxtLink rel="noopener" target="_blank" to="labo/color-palette-selector.html" class="effect labotext2 xl:text-5xl sm:text-4xl">
             Color Palette Selector
           </NuxtLink>
         </p>
         <p class="text-center elementlabo" data-aos="fade-up">
-          <NuxtLink rel="noopener" target="_blank" to="labo/concept-devis.html" class="effect labotext xl:text-6xl sm:text-4xl">
+          <NuxtLink rel="noopener" target="_blank" to="labo/concept-devis.html" class="effect labotext xl:text-5xl sm:text-4xl">
             Concept Devis
           </NuxtLink>
         </p>
@@ -152,6 +157,19 @@ p a {
 .labotext6 {
   text-align: center;
 	background-image: linear-gradient(to right, rgb(146, 68, 68), #ff4444, #ff0000, #ff4444, rgb(146, 68, 68));
+  background-size: 200% auto;
+  color: #000;
+  background-clip: text;
+  letter-spacing: 1px;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-animation: shine 3.5s linear infinite;
+          animation: shine 3.5s linear infinite;
+}
+
+.labotext7 {
+  text-align: center;
+	background-image: linear-gradient(to right, rgb(88, 100, 151), #4470ff, #1900ff, #4470ff, rgb(88, 100, 151));
   background-size: 200% auto;
   color: #000;
   background-clip: text;
