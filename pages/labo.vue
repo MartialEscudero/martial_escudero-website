7e038e9d6343b97aa06c4895542fb823751f83d0
Martial Escudero <martial.escudero@gmail.com>
2022-08-14 11:41:13 +0200

    twitter & og meta

--- pages/labo.vue
+++ pages/labo.vue
@@ -4,16 +4,16 @@ export default  {
         useHead({
             title: "Le Labo",
             meta: [
-                { name: "description", content: "Labo" },
+                { name: "description", content: "Le Labo de Martial Escudero - Développeur Front-End" },
 
-                { hid: "og:url",          property: "og:url",           content: "https://www.martialescudero.com/labo" },
-                { hid: "og:title",        property: "og:title",         content: "Le Labo | Martial Escudero" },
-                { hid: "og:description",  property: "og:description",   content: "Le Labo de Martial Escudero - Développeur Front-End." },
+                { property: "og:url", content: "https://www.martialescudero.com/labo" },
+                { property: "og:title", content: "Le Labo | Martial Escudero" },
+                { property: "og:description",   content: "Le Labo de Martial Escudero test1 - Développeur Front-End." },
 
 
-                { hid: "twitter:url",           property: "twitter:url",          content: "https://www.martialescudero.com/labo" },
-                { hid: "twitter:title",         property: "twitter:title",        content: "Le Labo | Martial Escudero"  },
-                { hid: "twitter:description",   property: "twitter:description",  content: "Le Labo de Martial Escudero - Développeur Front-End." },
+                { property: "twitter:url", content: "https://www.martialescudero.com/labo" },
+                { property: "twitter:title", content: "Le Labo | Martial Escudero"  },
+                { property: "twitter:description", content: "Le Labo de Martial Escudero test2 - Développeur Front-End." },
             ],
         })
     },
