742c1c32f26f0da0bbea5722ffc091440dcdce16
Martial Escudero <martial.escudero@gmail.com>
2023-01-21 16:10:35 +0100

    🔍 Update meta description

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@ useHead({
     meta: [
         { name: "author", content: "Martial Escudero" },
         { name: "copyright", content: "Martial Escudero" },
-        { name: "description", content: "Martial Escudero - Développeur Front-End - Site Portofolio et Labo." },
+        { name: "description", content: `Développeur Front-End de ${useGetAge()} ans avec ${getExp()} d'expérience professionnelle en Agence Web et diplômé d'une Licence Professionnelle en Technologie du Développement Web à l'Université de Limoges.`},
         { name: "keywords", content: "Martial Escudero,Escudero Martial,Nexton,NEXTON,consulting,agency,Étudiant,Université de Limoges,Limoges,LPMI,Licence Pro,DEUST,Webmaster & Métiers de l'Internet,Développeur Front-End,Développeur,Développeur Web,Vidéaste,Graphisme" },
 
         { property: "og:type", content: "website" },
@@ -31,6 +31,16 @@ useHead({
         { rel: "canonical", href: `https://www.martialescudero.com${route.fullPath !== "/" ? route.fullPath : ""}` }
     ]
 })
+
+function getExp() {
+    const date = new Date()
+    const currentYear = date.getFullYear()
+    const startYear = 2022
+
+    const exp = currentYear - startYear
+
+    return exp === 1 ? `${exp} an` : `${exp} ans`
+}
 </script>
 
 <template>
